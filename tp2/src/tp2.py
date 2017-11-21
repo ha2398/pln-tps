20928a94c8a9e36b9e075d73b6e3ffbb3dd11366
Hugo Sousa <hsousa@gmx.com>
2017-11-21 14:07:12 -0200

    tp2: add SVM

--- tp2/src/tp2.py
+++ tp2/src/tp2.py
@@ -8,9 +8,17 @@ tp2.py: Trabalho Prático II - Processamento de Linguagem Natural
 '''
 
 
+from sklearn import datasets
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
+from sklearn import svm
+
 import argparse as ap
 import numpy as np
 import sys
+
+
+vocab = {}
+tags = {}
 
 
 def parse_arguments():
@@ -39,6 +47,8 @@ def build_dataset(file):
 		@rtype:		Numpy array, Numpy array
 		'''
 
+	global vocab, tags
+
 	data = []
 	target = []
 
@@ -47,8 +57,18 @@ def build_dataset(file):
 
 		for raw_word in raw_words:
 			temp = raw_word.split('_')
-			data.append([temp[0].lower()])
-			target.append(temp[1])
+			token = temp[0].lower()
+			tag = temp[1]
+
+			if token not in vocab:
+				vocab[token] = len(vocab) + 1
+			
+			data.append([vocab[token]])
+
+			if tag not in tags:
+				tags[tag] = len(tags) + 1
+			
+			target.append(tags[tag])
 
 	data_array = np.array(data)
 	data_array.shape = (len(data), 1)
@@ -90,7 +110,7 @@ def read_data(train_filename, test_filename, validation_filename):
 	test_file.close()
 	validation_file.close()
 
-	return train_data, test_file, validation_file
+	return train_data, test_data, validation_data
 
 
 def main():
@@ -98,6 +118,26 @@ def main():
 	args = parse_arguments()
 	train_data, test_data, validation_data = \
 		read_data(args.train_file, args.test_file, args.validation_file)
+
+	print('Naive Bayes')
+	mnb = MultinomialNB()
+	predictor = mnb.fit(train_data[0], train_data[1])
+	y_pred = predictor.predict(test_data[0])
+	precision = ((test_data[1] == y_pred).sum()) / len(test_data[0])
+	print('Test:', (precision*100), '%')
+	y_pred = predictor.predict(validation_data[0])
+	precision = ((validation_data[1] == y_pred).sum()) / len(validation_data[0])
+	print('Validation:', (precision*100), '%')
+
+	print('SVM')
+	svmc = svm.SVC()
+	predictor = svmc.fit(train_data[0], train_data[1])
+	y_pred = predictor.predict(test_data[0])
+	precision = ((test_data[1] == y_pred).sum()) / len(test_data[0])
+	print('Test:', (precision*100), '%')
+	y_pred = predictor.predict(validation_data[0])
+	precision = ((validation_data[1] == y_pred).sum()) / len(validation_data[0])
+	print('Validation:', (precision*100), '%')
 
 
 main()
