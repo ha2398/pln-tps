6353ebca31e4a0d28c2094dc6f49f81bf19a217d
Hugo Sousa <hsousa@gmx.com>
2017-11-26 23:45:29 -0200

    tp2: add hash vectorizer

--- tp2/src/tp2.py
+++ tp2/src/tp2.py
@@ -8,33 +8,24 @@ tp2.py: Trabalho Prático II - Processamento de Linguagem Natural
 '''
 
 
-from sklearn import datasets
-from sklearn.naive_bayes import MultinomialNB, GaussianNB
-from sklearn import svm
-
 import argparse as ap
 import numpy as np
+from sklearn import svm
+from sklearn.feature_extraction.text import HashingVectorizer
+from sklearn.naive_bayes import MultinomialNB, GaussianNB
 import sys
 
 
-vocab = {}
-tags = {}
+# Add command line arguments to the program.
+parser = ap.ArgumentParser()
+parser.add_argument('train_file', type=str, help='Name of train file')
+parser.add_argument('test_file', type=str, help='Name of test file')
+parser.add_argument('validation_file', type=str,
+	help='Name of validation file')
+parser.add_argument('-s', dest='RSEED', default=0, type=int,
+	help='Random number generation seed')
 
-
-def parse_arguments():
-	''' Add command line arguments to the program.
-
-		@return:	Command line arguments.
-		@rtype:		argparse.Namespace.
-		'''
-
-	parser = ap.ArgumentParser()
-	parser.add_argument('train_file', type=str, help='Name of train file')
-	parser.add_argument('test_file', type=str, help='Name of test file')
-	parser.add_argument('validation_file', type=str,
-		help='Name of validation file')
-	return parser.parse_args()
-
+args = parser.parse_args()
 
 def build_dataset(file):
 	''' Read a file with words and their POS tags and create an array
@@ -47,31 +38,24 @@ def build_dataset(file):
 		@rtype:		Numpy array, Numpy array
 		'''
 
-	global vocab, tags
+	vec = HashingVectorizer(n_features=2, alternate_sign=False)
 
 	data = []
 	target = []
 
 	for line in file:
 		raw_words = line.split()
+		previous_tag = '-'
 
 		for raw_word in raw_words:
 			temp = raw_word.split('_')
 			token = temp[0].lower()
 			tag = temp[1]
+			data.append(' '.join([previous_tag, token]))
+			target.append(tag)
+			previous_tag = tag
 
-			if token not in vocab:
-				vocab[token] = len(vocab) + 1
-			
-			data.append([vocab[token]])
-
-			if tag not in tags:
-				tags[tag] = len(tags) + 1
-			
-			target.append(tags[tag])
-
-	data_array = np.array(data)
-	data_array.shape = (len(data), 1)
+	data_array = vec.transform(data).toarray()
 	target_array = np.array(target)
 
 	return data_array, target_array
@@ -115,29 +99,29 @@ def read_data(train_filename, test_filename, validation_filename):
 
 def main():
 
-	args = parse_arguments()
 	train_data, test_data, validation_data = \
 		read_data(args.train_file, args.test_file, args.validation_file)
 
+	print(train_data[0], train_data[1])
+	
 	print('Naive Bayes')
-	mnb = MultinomialNB()
-	predictor = mnb.fit(train_data[0], train_data[1])
-	y_pred = predictor.predict(test_data[0])
-	precision = ((test_data[1] == y_pred).sum()) / len(test_data[0])
-	print('Test:', (precision*100), '%')
+	gnb = GaussianNB()
+	predictor = gnb.fit(train_data[0], train_data[1])
 	y_pred = predictor.predict(validation_data[0])
 	precision = ((validation_data[1] == y_pred).sum()) / len(validation_data[0])
 	print('Validation:', (precision*100), '%')
+	y_pred = predictor.predict(test_data[0])
+	precision = ((test_data[1] == y_pred).sum()) / len(test_data[0])
+	print('Test:', (precision*100), '%')
 
 	print('SVM')
-	svmc = svm.SVC()
+	svmc = svm.SVC(random_state=args.RSEED)
 	predictor = svmc.fit(train_data[0], train_data[1])
-	y_pred = predictor.predict(test_data[0])
-	precision = ((test_data[1] == y_pred).sum()) / len(test_data[0])
-	print('Test:', (precision*100), '%')
 	y_pred = predictor.predict(validation_data[0])
 	precision = ((validation_data[1] == y_pred).sum()) / len(validation_data[0])
 	print('Validation:', (precision*100), '%')
-
+	y_pred = predictor.predict(test_data[0])
+	precision = ((test_data[1] == y_pred).sum()) / len(test_data[0])
+	print('Test:', (precision*100), '%')
 
 main()
